25a99e29730e2f332934ed67e7c26f4994de3afb
BrandonMFong <bfong@owc.com>
2021-10-31 17:53:53 +0000

    adding a private routing table

--- provider.tf
+++ provider.tf
@@ -67,33 +67,13 @@ resource "aws_internet_gateway" "week9-igw-v2" {
 }
 
 # Route Table
-resource "aws_route_table" "week9-rt-v2" {
+resource "aws_route_table" "week9-pri-rt" {
   vpc_id = aws_vpc.week9-vpc.id
 
-  route = [{
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.week9-igw-v2.id
-
-    # Values suggested by professor
-    egress_only_gateway_id    = ""
-    instance_id               = ""
-    ipv6_cidr_block           = ""
-    nat_gateway_id            = ""
-    network_interface_id      = ""
-    transit_gateway_id        = ""
-    vpc_endpoint_id           = ""
-    vpc_peering_connection_id = ""
-
-    # Values suggested by the validate process
-    # Investigate these values if there is an issue
-    # with our config 
-    carrier_gateway_id         = ""
-    destination_prefix_list_id = ""
-    local_gateway_id           = ""
-  }, ]
+  route = []
 
   tags = {
-    Name = "week9-rt"
+    Name = "week9-pri-rt"
   }
 }
 
