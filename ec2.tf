d2516611352f7cabe51a22bc4ba3f517ecf56845
BrandonMFong <bfong@owc.com>
2021-10-31 19:13:32 +0000

    Changing subnet

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "week9-bastion-vm" {
   ami           = "ami-02e136e904f3da870"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.week9-sub-a.id
+  subnet_id     = aws_subnet.week9-sub-b.id
 
   vpc_security_group_ids = [
     aws_security_group.week9-ssh-sg-v2.id
