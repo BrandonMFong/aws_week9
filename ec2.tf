510d5137984c9a53e30071d579f34c4458dba2ba
BrandonMFong <bfong@owc.com>
2021-10-31 18:48:52 +0000

    Adding worker vm

--- ec2.tf
+++ ec2.tf
@@ -14,3 +14,28 @@ resource "aws_instance" "week9-bastion-vm" {
     Name = "week9-bastion-vm"
   }
 }
+
+# worker vm
+resource "aws_instance" "week9-worker-vm" {
+  ami                  = "ami-02e136e904f3da870"
+  instance_type        = "t2.micro"
+  subnet_id            = aws_subnet.week9-pri-a.id
+  iam_instance_profile = aws_iam_instance_profile.week9-profile-ref.name 
+
+  vpc_security_group_ids = [
+    aws_security_group.week9-ssh-pri-sg.id
+  ]
+
+  key_name = "ECE592"
+
+  tags = {
+    Name = "week9-bastion-vm"
+  }
+}
+
+# IAM profile ref
+resource "aws_iam_instance_profile" "week9-profile-ref" {
+  name = "week9-profile-ref"
+  role = aws_iam_role.week9-role.name
+  tags = {}
+}
